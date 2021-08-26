36270e6af1e9fcdb2ae47d2c7e71e44c54494d03
MedZiedHaddar <medzied.haddar@gmail.com>
2021-08-26 13:57:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Hero from "./components/Hero";
+import {BrowserRouter as Router} from 'react-router-dom'
+import { GlobalStyle } from "./globalStyles";
+import Product from "./components/Products";
+import { productData,productDataTwo } from "./components/Products/data";
+import Feature from "./components/Feature";
+import Footer from "./components/Footer"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyle/>
+      <Hero/>
+      <Product heading='Choose your favorite Pizza' data={productData}/>
+      <Feature />
+      <Product heading='Sweet Treats for You ' data={productDataTwo}/>
+      <Footer/>
+
+    </Router>
+    
   );
 }
 
